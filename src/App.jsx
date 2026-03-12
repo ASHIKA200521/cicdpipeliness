dda1eb2bdfdbd98b9243c77fc16ef128273c9b07
Ashika Acharya <ashikaachar0@gmail.com>
2026-03-12 16:02:57 +0530

    Update heading text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-     <h1>heyy</h1> 
+     <h1>heyy asssshhh</h1> 
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
